03b64a43fdcd0f0a2d620ac9b8ab6e36418ea2a8
suthon <suthon@gmail.com>
2020-08-16 19:30:33 +0700

    initial

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={doIt}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
